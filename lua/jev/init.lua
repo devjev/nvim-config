362d2ca8f1ea726726524fbd036dcda0ad1f7822
Jevgeni Tarassov <jevgeni@tarassov.ch>
2023-12-18 14:18:34 +0100

    Update init.lua

--- lua/jev/init.lua
+++ lua/jev/init.lua
@@ -31,10 +31,10 @@ local builtin = require('telescope.builtin')
 -- Tab-based keys
 vim.keymap.set('n', '<Tab>', builtin.buffers, { noremap = True, silent = true })
 vim.keymap.set('n', '<S-Tab>', function() vim.cmd([[Neotree toggle]]) end, {noremap = true, silent = true})
-vim.keymap.set('n', '<S-ESC>', function() vim.cmd([[Telescope lsp_document_symbols]]) end, {noremap = true, silent = true})
+vim.keymap.set('n', '<leader>fd', function() vim.cmd([[Telescope lsp_document_symbols]]) end, {noremap = true, silent = true})
 
 -- Finding stuff
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>rn', function() return ':IncRename' .. vim.fn.expand('<cword>') end, {expr = true}) 
+-- vim.keymap.set('n', '<leader>rn', function() return ':IncRename' .. vim.fn.expand('<cword>') end, {expr = true}) 
