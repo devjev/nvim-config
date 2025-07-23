a6f0a2aa228f49760fe808c98edd8b9f9f04be6f
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-07-23 13:11:15 +0300

    Add automatic transparent background autocmd

--- init.lua
+++ init.lua
@@ -6,12 +6,32 @@ require("keybindings")
 vim.cmd("language en_US.UTF-8")
 
 -- Default colorscheme
-vim.cmd([[colorscheme lackluster]])
+vim.cmd([[colorscheme lackluster-dark]])
 
--- Remove background to allow default terminal background
--- to pass through
-vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+local function set_transparent_bg()
+  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+end
+
+-- Create an autocommand group to prevent stacking duplicates
+local transparent_augroup = vim.api.nvim_create_augroup('TransparentBG', { clear = true })
+
+-- Create the autocommand
+vim.api.nvim_create_autocmd(
+  -- A list of events to listen for
+  { 'ColorScheme', 'OptionSet' },
+  {
+    -- The pattern to match for each event
+    pattern = { '*', 'background' },
+    -- The function to call when the event fires
+    callback = set_transparent_bg,
+    -- The autocommand group to belong to
+    group = transparent_augroup,
+  }
+)
+
+-- Optional: Run it once on startup as well
+set_transparent_bg()
 
 -- Basics
 vim.opt.tabstop = 4
