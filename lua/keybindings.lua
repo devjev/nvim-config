965d3403eb371e1fc65cd9f338bbf519e10ee45c
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-07-25 11:59:47 +0300

    Fix keybindings for telescope buffers

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -5,7 +5,7 @@ local wk = require("which-key")
 -- Tab shortcuts
 wk.add {
     { "<Tab>", builtin.lsp_document_symbols, desc="Show all symbols in buffer", mode="n" },
-    { "S-<Tab>", builtin.buffers, desc="Show buffer", mode="n", noremap=true, silent=true }
+    { "S-<Tab>", builtin.buffers, desc="Show buffer", mode="n" }
 }
 
 -- Finding stuff
