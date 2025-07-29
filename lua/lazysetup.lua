e0184d96908da3d6b9f96b7d315bc97482248681
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-07-29 21:33:18 +0300

    Configure telescope

--- lua/lazysetup.lua
+++ lua/lazysetup.lua
@@ -26,7 +26,21 @@ require("lazy").setup {
 	{
 		"nvim-telescope/telescope.nvim",
 		branch = "0.1.x",
-		dependencies = { "nvim-lua/plenary.nvim" }
+		dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    layout_strategy = 'vertical',
+                    layout_config = {
+                        vertical = {
+                            prompt_position = 'top',
+                            preview_cutoff = 120,
+                            preview_height = 0.4,
+                        }
+                    }
+                }
+            })
+        end
 	},
 
 	-- Lualine (TODO need to customize this)
