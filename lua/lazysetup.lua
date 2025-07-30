8488e0496a793d1964184725c41860dddac4082f
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-07-30 10:06:06 +0300

    More layout tweaks

--- lua/lazysetup.lua
+++ lua/lazysetup.lua
@@ -31,7 +31,7 @@ require("lazy").setup {
             require("telescope").setup({
                 defaults = {
                     layout_strategy = "center",
-                    layout_config = { height = 0.95 },
+                    layout_config = { height = 0.45 },
                 }
             })
         end
