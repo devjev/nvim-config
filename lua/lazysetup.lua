8d416bd45453bcb96badc25225d589fce6361f71
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-07-30 10:23:23 +0300

    Add width to telescope layout

--- lua/lazysetup.lua
+++ lua/lazysetup.lua
@@ -31,7 +31,7 @@ require("lazy").setup {
             require("telescope").setup({
                 defaults = {
                     layout_strategy = "center",
-                    layout_config = { height = 0.36 },
+                    layout_config = { height = 0.36, width = 0.8 },
                 }
             })
         end
