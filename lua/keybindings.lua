f610b9a8d433982ea645f346649a2cc4389c4a57
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-07-31 12:30:27 +0300

    Change C-Esc keybinding to S-Esc
    To avoid Windows problems.

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -4,7 +4,7 @@ local wk = require("which-key")
 
 -- Tab shortcuts
 wk.add {
-    { "<C-Esc>", builtin.lsp_document_symbols, desc="Show all symbols in buffer", mode="n" },
+    { "<S-Esc>", builtin.lsp_document_symbols, desc="Show all symbols in buffer", mode="n" },
     { "<S-Tab>", function() builtin.buffers() end, desc="Show buffers", mode="n" }
 }
 
