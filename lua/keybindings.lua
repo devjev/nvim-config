003ca8ab285151c88f3283251e3f1ece74e51028
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-07-31 12:35:57 +0300

    Change S-Esc to F12 because fucking Windows

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -4,7 +4,7 @@ local wk = require("which-key")
 
 -- Tab shortcuts
 wk.add {
-    { "<S-Esc>", builtin.lsp_document_symbols, desc="Show all symbols in buffer", mode="n" },
+    { "<F12>", builtin.lsp_document_symbols, desc="Show all symbols in buffer", mode="n" },
     { "<S-Tab>", function() builtin.buffers() end, desc="Show buffers", mode="n" }
 }
 
@@ -64,7 +64,7 @@ wk.add {
     { "<F10>", function() require("dapui").float_element("breakpoints") end, desc="Show breakpoints", mode="n" },
     -- F11 should be set to something else
     { "<F11>", builtin.diagnostics, desc="Show diagnostics", mode="n" },
-    { "<F12>", builtin.lsp_definitions, desc="Show definitions", mode="n" },
+    -- { "<F12>", builtin.lsp_definitions, desc="Show definitions", mode="n" },
 }
 
 -- LSP & Code Actions
