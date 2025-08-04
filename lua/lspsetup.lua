02eb1e9aa79a4bb3d24309156c8f0091e5b7da56
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-08-04 15:58:33 +0300

    Ignore W503 for Python LSP

--- lua/lspsetup.lua
+++ lua/lspsetup.lua
@@ -69,7 +69,7 @@ lspconfig.pylsp.setup({
             plugins = {
                 pycodestyle = {
                     enabled = true,
-                    ignore = { "E501" },
+                    ignore = { "E501", "W503" },
                     maxLineLength = 120,
                 }
             }
