a3b8c4b27b0949613bbaec5c0fa280bcda382274
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-08-05 17:09:42 +0300

    Update keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -13,6 +13,7 @@ wk.add {
     { "<leader>ff", builtin.find_files, desc="Find file", mode="n" },
     { "<leader>fg", builtin.live_grep, desc="Grep files", mode="n" },
     { "<leader>fh", builtin.help_tags, desc="Find help", mode="n" },
+    { "<leader>fs", group = "Find LSP symbols..." },
     {
         "<leader>fss",
         function()
@@ -23,13 +24,15 @@ wk.add {
         mode="n"
     },
     { "<leader>fsu", builtin.lsp_references, desc="Find symbol use", mode="n" },
+    { "<leader>fc", builtin.commands, desc="Find (and execute) commands", mode={ "n", "v"}, },
+    { "<leader>fo", builtin.commands, desc="Find vim options", mode="n", },
 }
 
 -- Going places
 wk.add {
     { "<leader>g", group = "Go to..." },
     { "<leader>go", "<CMD>Oil<CR>", desc="Go to file manager", mode="n" },
-    { "<leader>gs", builtin.lsp_definitions, desc="Go to symbol definition", mode="n" },
+    { "<leader>gd", builtin.lsp_definitions, desc="Go to symbol definition", mode="n" },
 }
 
 -- Wiki
@@ -78,49 +81,71 @@ wk.add({
 	{ "<leader>qa", vim.lsp.buf.code_action, desc = "Code action", mode = { "n", "v" } },
 	{ "<leader>qd", builtin.diagnostics, desc = "Show workspace diagnostics", mode = "n" },
 	{ "<leader>qe", vim.diagnostic.open_float, desc = "Show line diagnostics", mode = "n" },
-    { "<leader>qvd", "<CMD>DiffviewOpen<CR>", desc="Show git diff", mode="n" },
-    { "<leader>qvc", "<CMD>Neogit<CR>", desc="Show git status", mode="n" },
+	{ "<leader>qf", builtin.quickfix, desc = "Show quickfix menu", mode = "n" },
+	{ "<leader>qF", builtin.quickfixhistory, desc = "Show quickfix history", mode = "n" },
+
+    -- Duplicate debugger keys to non-function keys
+    { "<leader>qd", group = "Debugger" },
+    { "<leader>qdd", require("dap").continue, desc="Run debugger to breakpoint", mode="n" },
+    { "<leader>qdv", require("dapui").toggle, desc="Show debugger UI", mode="n" },
+    { "<leader>qds", function() require("dapui").float_element("scopes") end, desc="Show scopes", mode="n" },
+    { "<leader>qdw", function() require("dapui").float_element("watches") end, desc="Show watches", mode="n" },
+    { "<leader>qdS", function() require("dapui").float_element("stacks") end, desc="Show stacks", mode="n" },
+
+    { "<leader>qdD", require("dap").close, desc="Stop debugger", mode="n" },
+    { "]D", require("dap").step_into, desc="Step into", mode="n" },
+    { "]d", require("dap").step_over, desc="Step over", mode="n" },
+
+    { "<leader>qdb", require("dap").toggle_breakpoint, desc="Toggle breakpoint", mode="n" },
+    { "<leader>qdB", function() require("dapui").float_element("breakpoints") end, desc="Show breakpoints", mode="n" }
 })
 
--- Gitsigns
+-- !GIT
 wk.add({
-    { "<leader>h", group = "Git" },
-    -- Hunk navigation
-    { "]c", function()
-        if vim.wo.diff then
-            return "]c"
-        end
-        vim.schedule(function()
-            require("gitsigns").next_hunk()
-        end)
-        return "<Ignore>"
-    end, desc = "Next Hunk", mode = "n", expr = true },
-    { "[c", function()
-        if vim.wo.diff then
-            return "[c"
-        end
-        vim.schedule(function()
-            require("gitsigns").prev_hunk()
-        end)
-        return "<Ignore>"
-    end, desc = "Previous Hunk", mode = "n", expr = true },
+    {
+        "]c",
+        function()
+            if vim.wo.diff then
+                return "]c"
+            end
+            vim.schedule(function()
+                require("gitsigns").next_hunk()
+            end)
+            return "<Ignore>"
+        end,
+        desc = "Next change",
+        mode = "n",
+        expr = true
+    },
+    {
+        "[c",
+        function()
+            if vim.wo.diff then
+                return "[c"
+            end
+            vim.schedule(function()
+                require("gitsigns").prev_hunk()
+            end)
+            return "<Ignore>"
+        end,
+        desc = "Previous change",
+        mode = "n",
+        expr = true
+    },
 
-    -- Actions
-    { "<leader>hs", function() require("gitsigns").stage_hunk() end, desc = "Stage Hunk", mode = { "n", "v" } },
-    { "<leader>hr", function() require("gitsigns").reset_hunk() end, desc = "Reset Hunk", mode = { "n", "v" } },
-    { "<leader>hS", require("gitsigns").stage_buffer, desc = "Stage Buffer", mode = "n" },
-    { "<leader>hu", require("gitsigns").undo_stage_hunk, desc = "Undo Stage Hunk", mode = "n" },
-    { "<leader>hR", require("gitsigns").reset_buffer, desc = "Reset Buffer", mode = "n" },
-    { "<leader>hp", require("gitsigns").preview_hunk, desc = "Preview Hunk", mode = "n" },
-    { "<leader>hb", function() require("gitsigns").blame_line({ full = true }) end, desc = "Blame Line", mode = "n" },
-    { "<leader>hd", require("gitsigns").diffthis, desc = "Diff This", mode = "n" },
-    { "<leader>hD", function() require("gitsigns").diffthis("~") end, desc = "Diff This ~", mode = "n" },
-    { "<leader>ht", group = "Toggle" },
-    { "<leader>htb", require("gitsigns").toggle_current_line_blame, desc = "Toggle Blame Line", mode = "n" },
-    { "<leader>htd", require("gitsigns").toggle_deleted, desc = "Toggle Deleted", mode = "n" },
-})
-
-wk.add({
-    { "<leader>v", group = "Git" },
-    { "<leader>vc", "<CMD>Neogit<CR>", desc="Show git status", mode="n" },
+    { "<leader>v", group = "Git..." },
+    { "<leader>vv", "<CMD>Neogit<CR>", desc="Show git status", mode="n" },
+    { "<leader>vc", "<CMD>DiffviewOpen<CR>", desc="Show git diff", mode="n" },
+    { "<leader>vs", function() require("gitsigns").stage_hunk() end, desc = "Stage change", mode = { "n", "v" } },
+    { "<leader>vr", function() require("gitsigns").reset_hunk() end, desc = "Reset change", mode = { "n", "v" } },
+    { "<leader>vS", require("gitsigns").stage_buffer, desc = "Stage entire buffer", mode = "n" },
+    { "<leader>vu", require("gitsigns").undo_stage_hunk, desc = "Undo stage change", mode = "n" },
+    { "<leader>vR", require("gitsigns").reset_buffer, desc = "Reset changes in buffer", mode = "n" },
+    { "<leader>vp", require("gitsigns").preview_hunk, desc = "Preview change", mode = "n" },
+    { "<leader>vb", function() require("gitsigns").blame_line({ full = true }) end, desc = "Blame line", mode = "n" },
+    { "<leader>vd", require("gitsigns").diffthis, desc = "Diff this", mode = "n" },
+    { "<leader>vD", function() require("gitsigns").diffthis("~") end, desc = "Diff this ~", mode = "n" },
+    { "<leader>vt", group = "Git toggle" },
+    { "<leader>vtb", require("gitsigns").toggle_current_line_blame, desc = "Toggle blame line", mode = "n" },
+    { "<leader>vtd", require("gitsigns").toggle_deleted, desc = "Toggle deleted", mode = "n" },
 })
