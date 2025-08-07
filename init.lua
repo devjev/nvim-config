15bfba080d5d62b654b2ca0b36e9a38b190ab821
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-08-07 12:42:27 +0300

    Add line number to gutter at cursor

--- init.lua
+++ init.lua
@@ -16,6 +16,7 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 vim.o.wrap = false
 vim.opt.scrolloff = 8
+vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = 'yes'
 vim.opt.showtabline = 1
