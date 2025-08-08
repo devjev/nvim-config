4fca4ce476ce5d8e04752366b31bf992838a5722
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-08-08 20:55:42 +0300

    Tweak rustaceanvim settings

--- lua/lazysetup.lua
+++ lua/lazysetup.lua
@@ -287,6 +287,9 @@ require("lazy").setup {
         'mrcjkb/rustaceanvim',
         version = '^6',
         lazy = false,
+        ["rust-analyzer"] = {
+            cargo = { allFeatures = true },
+        },
     },
 
 	-- Zig
