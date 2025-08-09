61c2adc1cdff6ea57de0aa680b26290609fbd5db
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-08-09 12:55:42 +0300

    Update light colorscheme

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ vim.cmd("language en_US.UTF-8")
 
 -- Colorschemes
 vim.g.dark_colorscheme  = "carbonfox"
-vim.g.light_colorscheme = "zenwritten"
+vim.g.light_colorscheme = "modus_vivendi"
 vim.cmd("colorscheme " .. vim.g.dark_colorscheme)
 
 -- Basics
