40faa7cdcc338f3fa87bc12caf7c297917ca39f9
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-12-09 08:24:51 +0100

    Add powershell options for windows use

--- init.lua
+++ init.lua
@@ -61,3 +61,12 @@ vim.diagnostic.config({
         },
     },
 })
+
+if is_windows then
+    vim.opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
+    vim.opt.Shellcmdflag = "-NoLogo -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    vim.opt.shellredir = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    vim.opt.shellquote = ""
+    vim.opt.shellxquote = ""
+end
