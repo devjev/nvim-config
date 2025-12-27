192ed9ea868b69c1a5448ca08de34a314406ba49
Jevgeni Tarassov <jevgeni@tarassov.ch>
2025-12-27 19:19:01 +0100

    Fix up init.lua

--- init.lua
+++ init.lua
@@ -62,10 +62,6 @@ vim.diagnostic.config({
     },
 })
 
--- Standard terminal prompt defaults for terminal mode
-vim.env.PS1 = "\\w > "
-vim.env.PROMPT = "%F{cyan}(nvim)%f %~ %# "
-
 -- Windows specific setting, making sure PowerShell plays nice with Neovim
 if is_windows then
     vim.opt.shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell"
