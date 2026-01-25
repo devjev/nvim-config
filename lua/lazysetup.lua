e38d0793e0c08ba0f90e0d4ed97d97c17953deb4
Jevgeni Tarassov <jevgeni@tarassov.ch>
2026-01-25 16:35:26 +0100

    Change: replace nvim-ufo with nvim-origamy

--- lua/lazysetup.lua
+++ lua/lazysetup.lua
@@ -100,15 +100,15 @@ require("lazy").setup({
 
     -- !FOLDING
     {
-        'kevinhwang91/nvim-ufo',
-        dependencies = 'kevinhwang91/promise-async',
-        config = function()
-            require('ufo').setup({
-                provider_selector = function(bufnr, filetype, buftype)
-                    return {'lsp', 'indent'}
-                end
-            })
-        end
+        "chrisgrieser/nvim-origami",
+        event = "VeryLazy",
+        opts = {}, -- needed even when using default config
+
+        -- recommended: disable vim's auto-folding
+        init = function()
+            vim.opt.foldlevel = 99
+            vim.opt.foldlevelstart = 99
+        end,
     },
 
 	-- !TELESCOPE
